6a4e92b92c8dff3f0358366c76ab2889b9f9380d
liewhw1 <liewhw1.work@gmail.com>
2017-03-14 19:21:12 +0800

    remove all files and commit again

--- stage2/miracle-zu/src/main/java/com/miracle/security/CredentialsDigest.java
+++
@@ -1,8 +0,0 @@
-package com.miracle.security;
-
-public interface CredentialsDigest {
-	public String digest(String plainCredentials, byte[] salt);
-
-	public boolean matches(String credentials, String plainCredentials,
-			byte[] salt);
-}
--- stage2/miracle-zu/src/main/java/com/miracle/zookeeper/IZKListener.java
+++
@@ -1,7 +0,0 @@
-package com.miracle.zookeeper;
-
-import org.apache.curator.framework.CuratorFramework;
-
-public interface IZKListener {
-	void executor(CuratorFramework client);
-}
